02c102a4236fda9974cbd3411d81be1370aa47b7
EPIC <45147725+Jishu-Epic@users.noreply.github.com>
2018-12-12 10:54:55 +0530

    Add files via upload

---
+++ banner.py
@@ -0,0 +1,32 @@
+#Banner Of Olaf
+
+header = '\033[95m'
+blue = '\033[94m'
+green = '\033[92m'
+yellow = '\033[93m'
+red = '\033[91m'
+white = '\033[0m'
+b = '\033[1m'
+cyan = '\033[36m'
+ul = '\033[4m'
+bheader = '\033[1;95m'
+bblue = '\033[1;94m'
+bgreen = '\033[1;92m'
+byellow = '\033[1;93m'
+bred = '\033[1;91m'
+bcyan = '\033[1;36m'
+backred = '\033[1;0;41m'
+
+
+
+banner = '''
+ /\\                 /\\
+/ \\'._   (\\_/)   _.'/ \\'''+green+'''   Made By 3P1C'''+white+'''
+|.''._'--('''+red+'''o'''+white+'''.'''+red+'''o'''+white+''')--'_.''.|
+ \_ / `;=/ " \\=;` \\ _/'''+blue+'''   T00L :- 0L4F'''+white+'''
+   `\__| \\___/ |__/`'''+yellow+'''    SkyKnight-Team'''+white+'''
+        \\(_|_)/
+         " ` "
+'''
+print(banner)
+print(bheader+"∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞")
